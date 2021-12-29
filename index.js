a46d3612a7c2fca5e307178541be762d226baeb5
sid2710-alt <73985972+sid2710-alt@users.noreply.github.com>
2021-12-29 14:48:34 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+app.use(expressLayouts);
+app.use(express.static('./assests'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 app.use('/',require('./routes/index'))
-app.use('view engine','ejs')
+app.set('view engine','ejs')
 app.set('views','./views')
 app.listen(port,function(err)
 {
